3d7184e685f61cf300cea978976caf9ec328b2b3
levanhoangtsth1977-crypto <levanhoangtsth1977@gmail.com>
2026-08-26 13:13:12 +0700

    Temporary repair

--- google-api-bridge.js
+++ google-api-bridge.js
@@ -3,5 +3,5 @@
 (function(){
 if(window.__LH_GOOGLE_BRIDGE_920__)return;
 window.__LH_GOOGLE_BRIDGE_920__=true;
-const CFG=Object.freeze({url:'https://script.google.com/macros/s/AKfycbxTPwf-jhrR8JOoKY5ZLuzlsDgcv3nWILtDPTrNWY5D...'});
+const CFG=Object.freeze({url:'https://script.google.com/macros/s/AKfycbxTPwf-jhrR8JOoKY5ZLuzlsDgcv3nWILtDPTrNWY5D3WILtDPTrNWY5D...'});
 })();
